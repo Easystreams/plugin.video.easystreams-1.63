3f0bd2c1b7dc0d8d015c77f1da9f6e70ce601751
Easystreams <info@easystreams.net>
2015-03-13 21:59:55 +0000

    Update default.py

--- default.py
+++ default.py
@@ -228,7 +228,7 @@ def get_xml_database(url, browse=False):
 
 
 def getCommunitySources(browse=False):
-        url = 'https://github.com/Easystreams/'
+        url = 'https://github.com/Easystreams/easystreams3.git/'
         soup = BeautifulSoup(makeRequest(url), convertEntities=BeautifulSoup.HTML_ENTITIES)
         files = soup('ul')[0]('li')[1:]
         for i in files:
